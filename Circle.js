c4c6af13047dcc250dfe39a12752cdb2cb6b7b14
aayanqazi <aayanqazi@yahoo.com>
2016-04-17 01:16:19 +0500

    NAME SPACE MODULE

---
+++ Circle.js
@@ -0,0 +1,6 @@
+module.exports={
+  pi:3.141592653589793238462643383279,
+  circumference: function(r){return 2*this.pi*r;},
+  area: function(r) {return 2*this.pi*r*r;
+  }
+};
---
+++ index.js
@@ -0,0 +1,3 @@
+var Circle = require('./Circle.js');
+console.log('c:'+Circle.circumference(5));
+console.log('a:'+Circle.area(5));
